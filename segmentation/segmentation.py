ddf9c1fa635274faf2d9e3692ea08520e8e873c7
unknown <adveysnet@gmail.com>
2025-03-22 11:48:40 -0400

    segmentation

--- segmentation/segmentation.py
+++ segmentation/segmentation.py
@@ -2,15 +2,16 @@ from transformers import pipeline
 from PIL import Image
 import requests
 
-def segment(file):
+def segment(file, imageno):
     # url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/segmentation_input.jpg"
     image = Image.open(requests.get(file, stream=True).raw)
 
     instance_segmentation = pipeline("image-segmentation", "facebook/mask2former-swin-large-cityscapes-instance")
-    pth = "C:\\Advey\\genai-genesis\\segmentation\\segment.jpg"
+    pth = "C:\\Advey\\genai-genesis\\segmentation\\segment" + str(imageno) + ".jpg"
     results = instance_segmentation(image)
 
-    seg = results[2]['mask']
+    seg = results[imageno]['mask']
+    label = results[imageno]['label']
 
     seg.save(pth)
 
