4b5da8a11339befb23606774b624c39998c278bb
unknown <adveysnet@gmail.com>
2025-03-22 21:23:42 -0400

    push

--- services/object-storage/main.py
+++ services/object-storage/main.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, request, jsonify
+import zipfile
+import io
+from flask import Flask, request, jsonify, send_file
 from google.cloud import storage
 
 app = Flask(__name__)
@@ -14,9 +16,11 @@ if not bucket_name:
 def upload_object(envid, filename):
     try:
         data = request.get_data()
+
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(f"testingenv/objects/{envid}/files/{filename}")
-        blob.upload_from_string(data)
+        blob.upload_from_string(data)  
+
         return jsonify({"message": "Object uploaded successfully"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -24,9 +28,12 @@ def upload_object(envid, filename):
 @app.route('/objects/<envid>/<filename>', methods=['GET'])
 def download_object(envid, filename):
     try:
+        
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(f"testingenv/objects/{envid}/files/{filename}")
-        data = blob.download_as_string()
+
+        data = blob.download_as_bytes() 
+
         return data, 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -34,20 +41,28 @@ def download_object(envid, filename):
 @app.route('/objects/', methods=['GET'])
 def download_objects():
     try:
+        # Get the bucket and the prefix for object listing
         bucket = storage_client.bucket(bucket_name)
         pref1 = "testingenv/objects"
         blob1 = bucket.list_blobs(prefix=pref1)
-        allStrs = []
-        for bl1 in blob1:
-            pref2 = f"testingenv/objects/{bl1.name}/files"
-            blob2 = bucket.list_blobs(prefix=pref2)
-            for bl2 in blob2:
-                if bl2.name.endswith(".urdf") or bl2.name.endswith(".obj"):
-                    data = bl2.download_as_string() 
-                    allStrs.append(data)
 
-        data = jsonify(allStrs)
-        return data, 200
+        # Create an in-memory ZIP file
+        zip_buffer = io.BytesIO()
+
+        with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+            # Iterate through the blobs to find .urdf and .obj files
+            for bl1 in blob1:
+                pref2 = f"testingenv/objects/{bl1.name}/files"
+                blob2 = bucket.list_blobs(prefix=pref2)
+                for bl2 in blob2:
+                    if bl2.name.endswith(".urdf") or bl2.name.endswith(".obj"):
+                        data = bl2.download_as_bytes()
+
+                        zip_file.writestr(bl2.name, data)
+
+        zip_buffer.seek(0)
+        return send_file(zip_buffer, as_attachment=True, download_name="all_files.zip", mimetype="application/zip")
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
