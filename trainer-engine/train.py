dc00509e42310cf09e938f03b3bbdff7226516ac
Jet Chiang <jetjiang.ez@gmail.com>
2025-03-23 05:18:48 -0400

    bug fix

--- trainer-engine/train.py
+++ trainer-engine/train.py
@@ -20,21 +20,21 @@ register(
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# def download_from_gcs(bucket_name, source_blob_name, destination_file_name):
-#     """Downloads a file from GCS."""
-#     storage_client = storage.Client()
-#     bucket = storage_client.bucket(bucket_name)
-#     blob = bucket.blob(source_blob_name)
-#     blob.download_to_filename(destination_file_name)
-#     logger.info(f"Downloaded {source_blob_name} to {destination_file_name}")
+def download_from_gcs(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a file from GCS."""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+    logger.info(f"Downloaded {source_blob_name} to {destination_file_name}")
 
-# def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
-#     """Uploads a file to GCS."""
-#     storage_client = storage.Client()
-#     bucket = storage_client.bucket(bucket_name)
-#     blob = bucket.blob(destination_blob_name)
-#     blob.upload_from_filename(source_file_name)
-#     logger.info(f"Uploaded {source_file_name} to {destination_blob_name}")
+def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to GCS."""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+    logger.info(f"Uploaded {source_file_name} to {destination_blob_name}")
 
 log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
 os.makedirs(log_dir, exist_ok=True)
