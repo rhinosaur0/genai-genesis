07f74d3907088840fae2dcb352a019b1405c4a06
unknown <adveysnet@gmail.com>
2025-03-22 21:10:44 -0400

    commit

--- services/object-storage/main.py
+++ services/object-storage/main.py
@@ -10,23 +10,43 @@ bucket_name = "genai-genesis-storage"
 if not bucket_name:
     raise ValueError("GCS_BUCKET_NAME environment variable must be set.")
 
-@app.route('/objects/<envid>/<id>', methods=['PUT'])
-def upload_object(envid, id):
+@app.route('/objects/<envid>/<filename>', methods=['PUT'])
+def upload_object(envid, filename):
     try:
         data = request.get_data()
         bucket = storage_client.bucket(bucket_name)
-        blob = bucket.blob(f"environments/{envid}/files/file"+str(id)+".data")
+        blob = bucket.blob(f"testingenv/objects/{envid}/files/{filename}")
         blob.upload_from_string(data)
         return jsonify({"message": "Object uploaded successfully"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/objects/<envid>/<id>', methods=['GET'])
-def download_object(envid, id):
+@app.route('/objects/<envid>/<filename>', methods=['GET'])
+def download_object(envid, filename):
     try:
         bucket = storage_client.bucket(bucket_name)
-        blob = bucket.blob(f"environments/{envid}/files/file"+str(id)+".data")
+        blob = bucket.blob(f"testingenv/objects/{envid}/files/{filename}")
         data = blob.download_as_string()
+        return data, 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/objects/', methods=['GET'])
+def download_objects():
+    try:
+        bucket = storage_client.bucket(bucket_name)
+        pref1 = "testingenv/objects"
+        blob1 = bucket.list_blobs(prefix=pref1)
+        allStrs = []
+        for bl1 in blob1:
+            pref2 = f"testingenv/objects/{bl1.name}/files"
+            blob2 = bucket.list_blobs(prefix=pref2)
+            for bl2 in blob2:
+                if bl2.name.endswith(".urdf") or bl2.name.endswith(".obj"):
+                    data = bl2.download_as_string() 
+                    allStrs.append(data)
+
+        data = jsonify(allStrs)
         return data, 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
